897169d2f7c714ca577ed22e962cf31970877925
Dopey <66791985+DopeyPepsee@users.noreply.github.com>
2022-01-05 14:39:56 +0200

    -added particles;

---
+++ New Version/lua/autorun/iv04_halo_3_particles_autorun.lua
@@ -0,0 +1,58 @@
+game.AddParticles( "particles/iv04_h3_blood.pcf" )
+game.AddParticles( "particles/iv04_h3_shield_effects.pcf" )
+game.AddParticles( "particles/iv04_h3_explosions.pcf" )
+game.AddParticles( "particles/iv04_h3_forerunner_explosions.pcf" )
+game.AddParticles( "particles/iv04_h3_covenant_vehicle_explosions.pcf" )
+game.AddParticles( "particles/iv04_h3_thruster_fx.pcf" )
+
+
+--Thruster Effects
+PrecacheParticleSystem( "iv04_halo_3_brute_jumppack_fx" )
+PrecacheParticleSystem( "iv04_halo_3_pelican_thruster_fx" )
+
+--Shield Effects
+PrecacheParticleSystem( "iv04_halo_3_jackal_shield_deplete" )
+PrecacheParticleSystem( "iv04_halo_3_jackal_shield_deplete_major" )
+PrecacheParticleSystem( "iv04_halo_3_brute_shield_critical" )
+PrecacheParticleSystem( "iv04_halo_3_brute_shield_deplete" )
+PrecacheParticleSystem( "iv04_halo_3_elite_shield_deplete" )
+PrecacheParticleSystem( "iv04_halo_3_elite_shield_deplete_arcs" )
+PrecacheParticleSystem( "iv04_halo_3_spartan_shield_deplete" )
+PrecacheParticleSystem( "iv04_halo_3_spartan_shield_deplete_arcs" )
+PrecacheParticleSystem( "iv04_halo_3_brute_shield_impact_effect" )
+PrecacheParticleSystem( "iv04_halo_3_elite_shield_impact_effect" )
+PrecacheParticleSystem( "iv04_halo_3_spartan_shield_impact_effect" )
+PrecacheParticleSystem( "iv04_halo_3_jackal_shield_impact_effect" )
+
+--Blood Effects
+
+PrecacheParticleSystem( "iv04_halo_3_blood_impact_human" )
+PrecacheParticleSystem( "iv04_halo_3_blood_impact_elite" )
+PrecacheParticleSystem( "iv04_halo_3_blood_impact_grunt" )
+PrecacheParticleSystem( "iv04_halo_3_blood_impact_jackal" )
+PrecacheParticleSystem( "iv04_halo_3_blood_impact_drone" )
+PrecacheParticleSystem( "iv04_halo_3_blood_impact_brute" )
+PrecacheParticleSystem( "iv04_halo_3_blood_impact_hunter" )
+
+--Gib effects
+PrecacheParticleSystem( "iv04_halo_3_flood_carrier_explode" )
+PrecacheParticleSystem( "iv04_halo_3_flood_gib_medium" )
+PrecacheParticleSystem( "iv04_halo_3_flood_gib_small" )
+
+--Explosion Effects
+PrecacheParticleSystem( "iv04_halo_3_monitor_damage_lvl1_fire" )
+PrecacheParticleSystem( "iv04_halo_3_monitor_damage_lvl2_fire" )
+PrecacheParticleSystem( "iv04_halo_3_sentinel_death_explosion" )
+PrecacheParticleSystem( "iv04_halo_3_explosion_engineer" )
+PrecacheParticleSystem( "iv04_halo_3_phantom_explosion" )
+PrecacheParticleSystem( "iv04_halo_3_scarab_explosion" )
+PrecacheParticleSystem( "iv04_halo_3_explosion_large" )
+
+game.AddDecal( "iv04_halo_3_blood_splat_hunter", "effects/halo_3/decals/blood_hunter" ) 
+game.AddDecal( "iv04_halo_3_blood_splat_elite", "effects/halo_3/decals/blood_elite" ) 
+game.AddDecal( "iv04_halo_3_blood_splat_jackal", "effects/halo_3/decals/blood_jackal" ) 
+game.AddDecal( "iv04_halo_3_blood_splat_grunt", "effects/halo_3/decals/blood_grunt" ) 
+game.AddDecal( "iv04_halo_3_blood_splat_brute", "effects/halo_3/decals/blood_brute" ) 
+game.AddDecal( "iv04_halo_3_blood_splat_drone", "effects/halo_3/decals/blood_drone" ) 
+game.AddDecal( "iv04_halo_3_blood_splat_engineer", "effects/halo_3/decals/blood_engineer" ) 
+game.AddDecal( "iv04_halo_3_blood_splat_flood", "effects/halo_3/decals/blood_flood" ) 
