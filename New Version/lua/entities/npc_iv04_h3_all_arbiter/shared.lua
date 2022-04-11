27ed467476ebb0eeef68488ebe8072db4fe9074e
DopeyPepsee <66791985+DopeyPepsee@users.noreply.github.com>
2022-04-11 14:03:26 +0300

    -added Arby;

--- New Version/lua/entities/npc_iv04_h3_all_arbiter/shared.lua
+++ New Version/lua/entities/npc_iv04_h3_all_arbiter/shared.lua
@@ -2,11 +2,11 @@ AddCSLuaFile()
 
 ENT.Base 			= "npc_iv04_h3_ai"
 
-ENT.PrintName = "Elite"
+ENT.PrintName = "Arbiter"
 
-ENT.Models = {"models/halo_3/characters/alliance/elite.mdl"}
+ENT.Models = {"models/halo_3/characters/alliance/arbiter.mdl"}
 
-ENT.StartHealth = 200
+ENT.StartHealth = 100
 
 ENT.Relationship = 1
 
@@ -24,25 +24,24 @@ ENT.PossibleWeapons = {
 	"astw2_halo3_plasmarifle",
 	"astw2_halo3_carbine",
 	"astw2_halo3_carbine",
-	"astw2_halo3_needler",
-	"astw2_halo3_needler"
-}
+	"astw2_halo3_energysword"
+	-- "astw2_halo3_flamethrower"
+} 
 
 function ENT:DoInit()
 	--self:SetBodygroup(1,math.random(-4,1))
 	--self:SetBodygroup(4,1)
 	--self:SetBodygroup(5,8)
-	self:SetSkin(math.random(1,2))
-	self:SetBodygroup(7,1)
 	--print(head)
-	self.VoiceType = "Elite_"..math.random(1,2)..""
+	self.VoiceType = "Arbiter"
 	self:SetName(""..self:GetClass().."_"..self:EntIndex().."")
 	self.Weapon:SetName(""..self.Weapon:GetClass().."_"..self.Weapon:EntIndex().."")
 	--print(self:GetName(),self.Weapon:GetName())
+	self.Unkillable = GetConVar("halo_3_nextbots_ai_heroes"):GetBool()
 end
 
-list.Set( "NPC", "npc_iv04_h3_all_elite_minor", {
-	Name = "Elite Minor",
-	Class = "npc_iv04_h3_all_elite_minor",
+list.Set( "NPC", "npc_iv04_h3_all_arbiter", {
+	Name = "Arbiter",
+	Class = "npc_iv04_h3_all_arbiter",
 	Category = "Halo 3"
 } )
